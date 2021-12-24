8b7ecdbec3a2e9d14262ab6a6a5ade2199f46213
Ferdi-the-best <ferdithebest123@gmail.com>
2021-12-25 00:04:55 +0100

    removed typo

--- src/main/java/com/github/ferdithedev/mcplayerapi/MinecraftPlayerAPI.java
+++ src/main/java/com/github/ferdithedev/mcplayerapi/MinecraftPlayerAPI.java
@@ -8,10 +8,10 @@ public class MinecraftPlayerAPI {
         if(trimmedUUID == null) throw new IllegalArgumentException();
         StringBuilder builder = new StringBuilder(trimmedUUID.trim());
         try {
-            builder.insert(20, "-");
-            builder.insert(16, "-");
-            builder.insert(12, "-");
             builder.insert(8, "-");
+            builder.insert(12, "-");
+            builder.insert(16, "-");
+            builder.insert(20, "-");
         } catch (StringIndexOutOfBoundsException e){
             throw new IllegalArgumentException();
         }
